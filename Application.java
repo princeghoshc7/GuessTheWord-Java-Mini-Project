9980e930963f9759c08db84f8417115bc0e6bc7e
Prince Ghosh <83350037+princeghoshc7@users.noreply.github.com>
2023-07-01 11:11:10 +0530

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,11 @@
+package Guess;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GuessTheWord game=new GuessTheWord();
+		game.start();
+	}
+
+}
